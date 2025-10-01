c9cfdc356b65242d0563141f568c1a49dc3da17f
ChikenduHillary <cikenduhill@gmail.com>
2025-10-01 13:07:49 +0100

    feat: implemented the footer

--- src/slices/VideoBlock/index.tsx
+++ src/slices/VideoBlock/index.tsx
@@ -1,6 +1,13 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
+import { Content, isFilled } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
+import clsx from "clsx";
+import { LazyYouTubePlayer } from "./LazyYouTubePlayer";
+import Image from "next/image";
+
+const MASK_CLASSES =
+  "[mask-image:url(/video-mask.png)] [mask-mode:alpha] [mask-position:center_center] [mask-repeat:no-repeat] [mask-size:100%_auto]";
 
 /**
  * Props for `VideoBlock`.
@@ -12,41 +19,46 @@ export type VideoBlockProps = SliceComponentProps<Content.VideoBlockSlice>;
  */
 const VideoBlock: FC<VideoBlockProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-texture bg-zinc-900"
     >
-      Placeholder component for video_block (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server@latest"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select a model optimized for coding (e.g. Claude 3.7 Sonnet or similar)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 🎙️ Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       * 📚 Documentation: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <h2 className="sr-only">Video Reel</h2>
+      <div className="relative aspect-video">
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-brand-lime absolute inset-0 ~translate-x-2/3 ~translate-y-2/3"
+          )}
+        />
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/3 ~translate-y-1/2"
+          )}
+        />
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/2 ~-translate-y-1/3"
+          )}
+        />
+
+        {/* Video */}
+        <div className={clsx(MASK_CLASSES, "relative h-full")}>
+          {isFilled.keyText(slice.primary.youtube_video_id) && (
+            <LazyYouTubePlayer youTubeID={slice.primary.youtube_video_id} />
+          )}
+          <Image
+            src="/image-texture.png"
+            alt=""
+            fill
+            className="pointer-events-none object-cover opacity-50"
+          />
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
