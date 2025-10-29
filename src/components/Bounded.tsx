2315e9cb2b9f0b1ff17ddd22af0522003b758b98
ChikenduHillary <cikenduhill@gmail.com>
2025-10-29 13:52:44 +0100

    feat: fixed error

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,26 +1,33 @@
-import { CSSProperties, ElementType, ReactNode } from "react";
+import React, {
+  CSSProperties,
+  ElementType,
+  ReactNode,
+  ComponentPropsWithoutRef,
+} from "react";
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: ElementType;
+type BoundedProps<C extends ElementType = "section"> = {
+  as?: C;
   className?: string;
   style?: CSSProperties;
   children: ReactNode;
-};
+} & Omit<ComponentPropsWithoutRef<C>, "className" | "style" | "children">;
 
-export function Bounded({
-  as: Comp = "section",
+export function Bounded<C extends ElementType = "section">({
+  as: Comp = "section" as C,
   className,
   children,
+  style,
   ...restProps
-}: BoundedProps) {
+}: BoundedProps<C>) {
   return (
     <Comp
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
         className
       )}
-      {...restProps}
+      style={style}
+      {...(restProps as any)}
     >
       <div className="mx-auto w-full max-w-6xl">{children}</div>
     </Comp>
